8442d24355c078699aa5c2fe4cc06b5ff679ef30
Gaurav Joshi <codingmeanlogic008@gmail.com>
2025-06-01 00:56:13 +0545

    Changed name from main to IO

---
+++ IO.dart
@@ -0,0 +1,10 @@
+// Day 1 -May 1, 2025
+import 'dart:io';
+void main()
+{
+// First program in Dart
+// IO in Dart
+print('Enter a number:\n'); // To print, no dependencies
+int? num=int.parse(stdin.readLineSync()!);
+print("You entered $num");
+}
